e7f434a54e2ab8ffc11de28ea64af74f6be77e82
Rachel Wong <rwrachelwong@gmail.com>
2018-09-16 00:44:14 +0000

    completed loops exercise

--- if-statement.js
+++ if-statement.js
@@ -1,5 +1,5 @@
 var fruit = 'orange';
-if (fruit > 5) {
+if (fruit.length > 5) {
   console.log('The fruit name has more than five characters.');
 } else {
   console.log('The fruit name has five characters or less.');
